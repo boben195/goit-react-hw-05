48fb4cc71de84d60e71201da34673ffd7c1436da
boben195 <boben195@ukr.net>
2024-04-10 20:21:49 +0300

    remembr

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,14 +10,14 @@ const getNavLinkClass = ({ isActive }) =>
 const Navigation = () => {
   return (
     <header className={css.header}>
-      <div className={css.headerLink}>
+      
         <NavLink className={getNavLinkClass} to="/">
           HOME
         </NavLink>
         <NavLink className={getNavLinkClass} to="/movies">
           MOVIES
         </NavLink>
-      </div>
+      
     </header>
   );
 };
